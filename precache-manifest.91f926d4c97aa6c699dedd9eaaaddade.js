8037ddafbcf80530fccd11af6e1c85c45f5efa67
Naman <vishnoi_naman@yahoo.co.in>
2018-12-13 23:25:20 -0500

    Updates

---
+++ precache-manifest.91f926d4c97aa6c699dedd9eaaaddade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "21e5ede0774dc5fcf4d5",
+    "url": "/contactmanager/static/js/main.21e5ede0.chunk.js"
+  },
+  {
+    "revision": "e38315d895b4f1bd1ae4",
+    "url": "/contactmanager/static/js/1.e38315d8.chunk.js"
+  },
+  {
+    "revision": "21e5ede0774dc5fcf4d5",
+    "url": "/contactmanager/static/css/main.2e2fbfad.chunk.css"
+  },
+  {
+    "revision": "e38315d895b4f1bd1ae4",
+    "url": "/contactmanager/static/css/1.948e3e29.chunk.css"
+  },
+  {
+    "revision": "4dee5950eb8d878eaf841d1c3e0bbda8",
+    "url": "/contactmanager/index.html"
+  }
+];
